e61e860227e198a0ab0e0bd4ee33e25e5a31684b
wanjun <49808814+wanjun927@users.noreply.github.com>
2022-12-20 07:03:28 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
  * @Author: Mia
  * @Date: 2022-12-19 07:00:44
  * @LastEditors: Mia
- * @LastEditTime: 2022-12-19 07:52:02
+ * @LastEditTime: 2022-12-20 06:59:47
  * @Description: 
  */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isred, setIsRed] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{backgroundColor: isred ? 'red' : 'black'}}>
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
@@ -19,6 +21,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
+          <button onClick={() => setIsRed(!isred)}>change background color</button>
           <a
             className="App-link"
             href="https://reactjs.org"
